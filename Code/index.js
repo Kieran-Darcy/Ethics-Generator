7f371be07a2b9ae12b7be116ace4aeaa64ae367a
Kieran <kieran.darcy1@gmail.com>
2020-02-12 13:05:06 +0000

    - made node server work with database instead of script.js

--- Code/index.js
+++ Code/index.js
@@ -1,45 +1,72 @@
 const express = require('express');
 const mysql = require('mysql');
-const script = require('./Scripts/script.js');
 const app = express();
 const port = 80;
 app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: true}));
 
-/*// Create connection
+// Create connection
 const connection = mysql.createConnection({
-    host: "192.168.1.1",
+    host: "94.0.241.58",
     user: "user",
     password: "password",
-    database: "ethicsgenerator"
+    database: "ethicsgenerator",
+    multipleStatements: true
 });
 
-// Connect and return result of query
-function connect() {
-    connection.connect(err => {
-        if (err) throw err;
-        console.log("Connected to database");
-    });
+function randomPeople() {
+    let randPeople = [];
+    // picks 10 random people
+    for(let i = 0; i < 10; i++) {   // pick 10 people randomly
+        randPeople.push(people[Math.ceil(Math.random() * (people.length-1))]);
+    }
+    const variation = Math.round(Math.random()*2); // chooses a random number between 0 - 9
+    const peopleA = randPeople.splice(0, 4+variation); // split into group A
+    return {groupA : peopleA, groupB : randPeople};  // return groups
 }
 
 // Complete query
-function query(statement) {
-    connection.query(statement, (err, result) => {
+function query(statement, request) {
+    connection.query(statement, (err, results) => {
         if (err) throw err;
-        return result
+        return request(results);
+    });
+}
+
+function getPeople(numOfPeople = 10, response) {
+    query(`SELECT * FROM person ORDER BY RAND() LIMIT ${numOfPeople}`, results => {
+        return response(results)
+    });
+}
+
+function getCrossing(response) {
+    query('SELECT * FROM crossings ORDER BY RAND() LIMIT 1', result => {
+        return response(result[0].crossings)
+    });
+}
+
+// Make a scene for 2 group Scenarios
+function makeScene(response) {
+    getPeople(10, people => {
+        getCrossing(result => {
+            return response({
+                people: randomPeople(people),  //  {groupA : [GroupA], groupB : [GroupB]}
+                crossingType: result[0].crossings,  // crossing / green light / red light
+                timer: Math.random() >= 0.5
+            })
+        })
     })
 }
 
 // Insert people to the table
-function insertPeople() {
-    connect();
-    people.forEach(person => {
+/*function insertPeople() {
+    script.createPeople().forEach(person => {
         let statement = `INSERT INTO person (age, gender, profession, disability) VALUES ('${person.age}', '${person.gender}', '${person.profession}', '${person.disability}')`;
         console.log(query(statement))
     });
     console.log("Statement complete!");
-    connection.end()
 }*/
+
 
 app.get('/', (req, res) => {
     res.sendFile('Scenario1.html', {root: __dirname})
@@ -50,9 +77,9 @@ app.get('/CSS/background.png', (req, res) => {
 });
 
 app.get('/scene', (req, res) => {
-    const scene = script.makeScene();
-    console.log(scene);
-    res.send(scene);
+    makeScene(results => {
+        res.send(results)
+    })
 });
 
 app.post('/choice', (req, res) => {
